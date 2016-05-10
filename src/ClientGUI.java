786e6bca1160f2af236c6e27e7a5479d61bd8ff5
rhys-saldanha <rhys.saldanha@gmail.com>
2016-05-10 21:41:46 +0100

    Creates makeLoginPage method

--- src/ClientGUI.java
+++ src/ClientGUI.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -16,7 +17,132 @@ public class ClientGUI
 
 	public void init()
 	{
-		setPanel(makeButtonPanel());
+		setPanel(makeLogin());
+	}
+
+	public JPanel makeLogin()
+	{
+		JPanel p = new JPanel();
+		p.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
+		p.setLayout(new GridBagLayout());
+		GridBagConstraints gbc = new GridBagConstraints();
+
+		gbc.gridwidth = 2;
+		gbc.insets = new Insets(0, 200, 0, 200);
+		gbc.weightx = 0.5;
+		gbc.fill = GridBagConstraints.HORIZONTAL;
+		gbc.gridx = 0;
+
+		JLabel l_login = new JLabel("LOGIN");
+		gbc.gridy++;
+		p.add(l_login, gbc);
+
+		JTextField tf_username = new JTextField("Username");
+		gbc.gridy++;
+		p.add(tf_username, gbc);
+
+		JTextField tf_password = new JPasswordField("Password");
+		gbc.gridy++;
+		p.add(tf_password, gbc);
+
+		gbc.gridwidth = 1;
+		gbc.insets = null;
+
+		JButton b_login = new JButton("Login");
+		gbc.gridy++;
+		gbc.insets = new Insets(0, 200, 0, 20);
+		p.add(b_login, gbc);
+
+		JButton b_newUser = new JButton("New User");
+		gbc.gridx++;
+		gbc.insets = new Insets(0, 20, 0, 200);
+		p.add(b_newUser, gbc);
+
+		Border bor_default = (new JTextField()).getBorder();
+
+		b_login.addActionListener(new ActionListener()
+		{
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+				if (tf_username.getText().equals("")) {
+					tf_username.setBorder(BorderFactory.createLineBorder(Color.RED, 1));
+				} else {
+					tf_username.setBorder(bor_default);
+				}
+				if (tf_password.getText().equals("")) {
+					tf_password.setBorder(BorderFactory.createLineBorder(Color.RED, 1));
+				} else {
+					tf_password.setBorder(bor_default);
+				}
+				if (!tf_username.getText().equals("") && !tf_password.getText().equals("")) {
+
+				}
+			}
+		});
+
+		b_newUser.addActionListener(new ActionListener()
+		{
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+				setPanel(makeNewUserPanel());
+			}
+		});
+
+		/*button = new JButton("5");
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.ipady = 0;       //reset to default
+		c.weighty = 1.0;   //request any extra vertical space
+		c.anchor = GridBagConstraints.PAGE_END; //bottom of space
+		c.insets = new Insets(10,0,0,0);  //top padding
+		c.gridx = 1;       //aligned with button 2
+		c.gridwidth = 2;   //2 columns wide
+		c.gridy = 2;       //third row
+		p.add(button, c);*/
+
+		return p;
+	}
+
+	private JPanel makeNewUserPanel()
+	{
+		JPanel p = new JPanel();
+		p.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
+		p.setLayout(new GridBagLayout());
+		GridBagConstraints gbc = new GridBagConstraints();
+
+		gbc.gridwidth = 2;
+		gbc.insets = new Insets(0, 200, 0, 200);
+		gbc.weightx = 0.5;
+		gbc.fill = GridBagConstraints.HORIZONTAL;
+		gbc.gridx = 0;
+
+		JLabel l_login = new JLabel("LOGIN");
+		gbc.gridy++;
+		p.add(l_login, gbc);
+
+		JTextField tf_username = new JTextField("Username");
+		gbc.gridy++;
+		p.add(tf_username, gbc);
+
+		JTextField tf_password = new JPasswordField("Password");
+		gbc.gridy++;
+		p.add(tf_password, gbc);
+
+		gbc.gridwidth = 1;
+		gbc.insets = null;
+
+		JButton b_login = new JButton("Login");
+		gbc.gridy++;
+		gbc.insets = new Insets(0, 200, 0, 20);
+		p.add(b_login, gbc);
+
+		JButton b_newUser = new JButton("New User");
+		gbc.gridx++;
+		gbc.insets = new Insets(0, 20, 0, 200);
+		p.add(b_newUser, gbc);
+
+		return p;
 	}
 
 	public JPanel makeRandomPanel()
@@ -125,7 +251,6 @@ public class ClientGUI
 	public void setPanel(JPanel p)
 	{
 		f.setContentPane(p);
-		f.revalidate();
 		f.repaint();
 	}
 
